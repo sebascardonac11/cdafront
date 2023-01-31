283d79c4afdf6582faed77b24e0acca4d0a33ac2
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-01-31 16:40:48 -0500

    Create Portal

--- src/Home/header/index.js
+++ src/Home/header/index.js
@@ -3,18 +3,18 @@ import { Button } from 'react-bootstrap';
 function Home() {
     return (
         <React.Fragment>
-            <nav class="py-2 bg-light border-bottom">
-                <div class="container d-flex flex-wrap">
-                    <ul class="nav me-auto">
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2">Features</a></li>
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2">Pricing</a></li>
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2">FAQs</a></li>
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2">About</a></li>
+            <nav className="py-2 bg-light border-bottom">
+                <div className="container d-flex flex-wrap">
+                    <ul className="nav me-auto">
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2">Features</a></li>
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2">Pricing</a></li>
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2">FAQs</a></li>
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2">About</a></li>
                     </ul>
-                    <ul class="nav">
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2">Login</a></li>
-                        <li class="nav-item"><a href="https://gi.com" class="nav-link link-dark px-2">Sign up</a></li>
+                    <ul className="nav">
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2">Login</a></li>
+                        <li className="nav-item"><a href="https://gi.com" className="nav-link link-dark px-2">Sign up</a></li>
                     </ul>
                 </div>
             </nav>
