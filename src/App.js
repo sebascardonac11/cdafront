26eb11b08431da079a8092cc578552ab752e4475
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-01-30 19:26:38 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Button } from 'react-bootstrap';
+import React from 'react';
+function App() {
+  return (
+    <React.Fragment>
+      <header className='App-header'>
+        <h1>Fist react</h1>
+        <Button variant="primary">Acceder</Button>
+      </header>
+    </React.Fragment>
+  );
+}
+export default App;
